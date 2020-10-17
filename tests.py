920bc1b8c2da76c54a58f87c6e2940997b4593b9
delbarital <delbarital@gmail.com>
2020-10-18 00:49:31 +0300

    refactor the test file after changing the package name to hoopoe from enrich

--- tests.py
+++ tests.py
@@ -1,22 +1,22 @@
-import enrich
+import hoopoe
 import pytest
 
 
 # print(enrich.enrich("asda", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full"))
 
 def test_us_state_name_abbr_to_full_name():
-    assert enrich.enrich("ca", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "california"
-    assert enrich.enrich("wa", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "washington"
-    assert enrich.enrich("NY", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "new york"
-    assert enrich.enrich("nY", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "new york"
+    assert hoopoe.enrich("ca", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "california"
+    assert hoopoe.enrich("wa", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "washington"
+    assert hoopoe.enrich("NY", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "new york"
+    assert hoopoe.enrich("nY", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "new york"
     with pytest.raises(KeyError) as excinfo:
-        enrich.enrich("n", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full")
+        hoopoe.enrich("n", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full")
         assert "KeyError" in str(excinfo.value)
     with pytest.raises(KeyError) as excinfo:
-        enrich.enrich(" ", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full")
+        hoopoe.enrich(" ", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full")
         assert "KeyError" in str(excinfo.value)
     with pytest.raises(ValueError) as excinfo:
-        enrich.enrich(source_data_type="us_state_name_abbr", target_data_type="us_state_name_full")
+        hoopoe.enrich(source_data_type="us_state_name_abbr", target_data_type="us_state_name_full")
         assert "ValueError" in str(excinfo.value)
     
 
