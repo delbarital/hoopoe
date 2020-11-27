b76b6e25414c8e09412d0e08d1f29cc8d1515384
delbarital <delbarital@gmail.com>
2020-11-27 01:47:37 +0100

    adding missing comma

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup (
     name='hoopoe',
     version='0.0.1',
-    description='Hoopoe is a data enrichment service that makes it easy to improve your data-centric services and increase the accuracy of your ML models.'
+    description='Hoopoe is a data enrichment service that makes it easy to improve your data-centric services and increase the accuracy of your ML models.',
     py_modules=["hoopoe"],
     package_dir={'': 'src'}
 )
