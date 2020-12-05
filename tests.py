42a035c084715c8e31345ae5960ae59c5777d2af
delbarital <delbarital@gmail.com>
2020-12-05 19:17:55 +0100

    test cover for us state full name to state abbr

--- tests.py
+++ tests.py
@@ -17,5 +17,22 @@ def test_us_state_name_abbr_to_full_name():
         assert "ValueError" in str(excinfo.value)
     
 
+def test_us_state_full_name_to_state_abbr():
+    assert hoopoe.enrich("california", source_data_type="us_state_name_full", target_data_type="us_state_name_abbr") == "ca"
+    assert hoopoe.enrich("washington", source_data_type="us_state_name_full", target_data_type="us_state_name_abbr") == "wa"
+    assert hoopoe.enrich("New-york", source_data_type="us_state_name_full", target_data_type="us_state_name_abbr") == "ny"
+    assert hoopoe.enrich("NEW jersey", source_data_type="us_state_name_full", target_data_type="us_state_name_abbr") == "nj"
+    assert hoopoe.enrich("TEXAS", source_data_type="us_state_name_full", target_data_type="us_state_name_abbr") == "tx"
+    assert hoopoe.enrich("NORTH CAROLINA", source_data_type="us_state_name_full", target_data_type="us_state_name_abbr") == "nc"
+
+    with pytest.raises(KeyError) as excinfo:
+        hoopoe.enrich("n", source_data_type="us_state_name_full", target_data_type="us_state_name_abbr")
+        assert "KeyError" in str(excinfo.value)
+    with pytest.raises(KeyError) as excinfo:
+        hoopoe.enrich(" ", source_data_type="us_state_name_name", target_data_type="us_state_name_abbr")
+        assert "KeyError" in str(excinfo.value)
+    with pytest.raises(ValueError) as excinfo:
+        hoopoe.enrich(source_data_type="us_state_name_name", target_data_type="us_state_name_abbr")
+        assert "ValueError" in str(excinfo.value)
 
 # print(test_us_state_name_abbr_to_full_name())
